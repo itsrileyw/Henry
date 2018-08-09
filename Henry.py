122389c379c37831142d4e2af08200c2f7a74488
Indoors420 <38793945+Indoors420@users.noreply.github.com>
2018-08-08 19:50:39 -0700

    Update Henry.py

--- Henry.py
+++ Henry.py
@@ -7,8 +7,8 @@ async def on_ready(): #Responsible for actually sending the shitposts to a disco
         msg = shitpost()
         HenrysServer = bot.get_server(os.getenv("HENRYSSERVER"))
         await bot.send_message(HenrysServer.get_channel(os.getenv("HENRYSSERVER-GENERAL")), msg)
+        print("?")
         for _ in range(0,3601):
-            print("?")
             await asyncio.sleep(1)
 @bot.event
 async def on_command_error(error: Exception, ctx: commands.Context):
