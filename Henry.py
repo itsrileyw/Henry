e187c009ce49a8baa963559556cd9d5e3647cc9c
Indoors420 <38793945+Indoors420@users.noreply.github.com>
2018-08-09 16:56:05 -0700

    Update Henry.py

--- Henry.py
+++ Henry.py
@@ -1,4 +1,4 @@
-import discord, random, asyncio, datetime, os, lists
+import discord, random, asyncio, datetime, os, Lists
 from discord.ext import commands
 bot = commands.Bot(command_prefix="Henry, ")
 @bot.event
