0521ec0f98f4ce4198507c94e40e0881a486ba93
Indoors420 <38793945+Indoors420@users.noreply.github.com>
2018-08-14 20:46:11 -0700

    Update Lists.py

--- Lists.py
+++ Lists.py
@@ -271,7 +271,6 @@ retaliationNouns = [ #Nouns to use when responding to someone
     "your peepee",
 ]
 phrases = [
-    "These fuck niggas aren't ready for me to get on my yee haw shit",
     "Girls don't want luxury handbags and expensive clothes they want frag grenades and illegal firearms",
     "Girls think it's sweet when u throw a fucking flash bang grenade thru their window in the morning to wake them up for work",
     "White people be like 'oh yea ur cruisin for a bruisin bud",
